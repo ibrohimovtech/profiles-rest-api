ff737e6c53dd996d36e67c194fb8fc5c847c59a5
ibrohimovtech <ibrohimceo@gmail.com>
2023-05-31 01:19:09 +0500

    Configured Vagrant and setup hello world script.

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+x = int(input('Type your age: '))
+
+if x < 18:
+    print('You are younger than me.')
+elif x > 18:
+    print('You are older than me.')
+else:
+    print('We are in the same age.')
